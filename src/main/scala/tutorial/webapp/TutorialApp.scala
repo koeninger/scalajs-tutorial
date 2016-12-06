9bc07137c58a2eb5bb4bec624418d568412fd8e5
cody koeninger <cody@koeninger.org>
2016-12-05 21:58:33 -0600

    example usage of Dynamic.global

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,6 +2,7 @@ package tutorial.webapp
 
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.Dynamic.global
 
 import org.scalajs.dom
 import dom.document
@@ -9,6 +10,9 @@ import dom.document
 object TutorialApp extends JSApp {
   def main(): Unit = {
     appendPar(document.body, "Hello World")
+    global.document.someJsFun()
+    appendPar(document.body, global.someJsGloba.asInstanceOf[String])
+    global.someScalaGlobal = "some global from Scala"
   }
 
   def appendPar(targetNode: dom.Node, text: String): Unit = {
